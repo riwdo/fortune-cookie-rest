2df80138ff2ce3e04bf8e35c50d324efcda27623
Oscar Hansson <racso.hansson@gmail.com>
2023-12-30 14:27:47 +0100

    Init project

---
+++ src/main/java/com/good/fortunecookierest/FortuneCookieRestApplication.java
@@ -0,0 +1,13 @@
+package com.good.fortunecookierest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortuneCookieRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FortuneCookieRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/good/fortunecookierest/FortuneCookieRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.good.fortunecookierest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneCookieRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
