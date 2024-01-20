17e3a9de99fa25abe9e46f69d5dd869c6007f130
Oscar Hansson <oscarhansson@Oscars-MBP.home>
2024-01-20 09:27:47 +0100

    Example 1: Check

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # fortune-cookie-rest
-A simple Spring Boot rest api serving fortunes
-
+A simple Spring Boot rest api serving fortunes. This is intended to demonstrate the usefulness
+of using testcontainers when writing integration tests so will include several bad examples
+of how to not do.
 
 ## Running the application
 run `docker-compose up` to start kafka and postgresql
--- src/main/resources/db/testdata/V2_0_0__aftermigrate.sql
+++ src/main/resources/db/testdata/V2_0_0__aftermigrate.sql
@@ -1,11 +1,8 @@
 insert into fortune (created_at, description, author)
-values ('2023-12-30 12:30:00', 'Some fortune', 'John Doe');
+values ('2023-12-30 12:30:00', 'Merge with compassion, conflicts as opportunities. Let conflicts transform into resolutions. Merge mindfully.', 'Merge Master PeacefulMerge');
 
 insert into fortune (created_at, description, author)
-values ('2024-01-08 08:30:00', 'Some other fortune', 'John Doe');
+values ('2024-01-08 08:30:00', 'Release attachments to outdated tech. Clinging to the past hinders progress. Embrace present tools for a brighter coding future.', 'Tech Liberation ZenCoder');
 
 insert into fortune (created_at, description, author)
-values ('2024-01-12 17:30:00', 'Some massive fortune', 'John Sena');
-
-insert into fortune (created_at, description, author)
-values ('2024-01-12 17:30:00', 'Some massive second fortune', 'John Sena');
+values ('2024-01-12 17:30:00', 'Reflect on the Eightfold Path of Clean Code: Right Intent, Right Structure, Right Naming. Let your code embody these principles.', 'Code Monk PureIntent');
